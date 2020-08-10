00d97865e02a463d60e883cfaeac6f6f09980e68
Yong-hao Khor (AP) <fyhao1@gmail.com>
2020-08-11 00:28:47 +0800

    init dev.properties env

--- src/main/java/com/fyhao/springjpadbdiff/dbdiff/DBDiff.java
+++ src/main/java/com/fyhao/springjpadbdiff/dbdiff/DBDiff.java
@@ -1,23 +1,44 @@
 package com.fyhao.springjpadbdiff.dbdiff;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DBDiff {
 
+	static Logger log = LoggerFactory.getLogger(DBDiff.class);
 	String repo;
 	String path;
 	String sourceCommit;
 	String targetCommit;
+	
+	Properties prop = null;
+	
 	public DBDiff(String repo, String path, String sourceCommit, String targetCommit) {
 		this.repo = repo;
 		this.path = path;
 		this.sourceCommit = sourceCommit;
 		this.targetCommit = targetCommit;
+		prop = new Properties();
+		try {
+			prop.load(new FileInputStream("dev.properties"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void analyze() {
-		
+		String env = prop.getProperty("env");
+		log.info("analyze env " + env);
+		if(env.equals("dev")) {
+			
+		}
 	}
 	
 	public List<String> getOutputSQL() {
